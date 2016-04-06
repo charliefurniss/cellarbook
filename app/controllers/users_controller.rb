facc654059a156ee17b2803c28175e90b0ed3aa4
Charlie Furniss <charliefurniss96@gmail.com>
2016-04-06 16:18:09 +0100

    added links to wines index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,66 +2,48 @@
 
 class UsersController < ApplicationController
   before_action :authenticate, only: [:show]
+  
   def show
-
     @user = current_user
-    
   end
 
   def new
     #calls method defined in application controller
     if logged_in?
-
       redirect_to root_path
-
     end
 
     @user = User.new
-  
+
   end
 
   def create
-
     if logged_in?
-
       redirect_to root_path
-
     end
 
     @user = User.new(user_params)
 
     if @user.save
-
       redirect_to user_path
-
     else
       # shows the user the completed form again
       render "new"
-
     end
-    
   end
 
   def edit
-
     @user = current_user
-    
   end
 
   def update
-
     current_user.update(user_params)
-
     redirect_to user_path
-
   end  
 
   private
-
   def user_params
-
     params.require(:user).permit(:email, :password, :password_confirmation)
-
   end 
 
 end
