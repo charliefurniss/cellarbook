824a8163d525fc844aa0cde694191f6663ab339a
Charlie Furniss <charliefurniss96@gmail.com>
2016-04-07 16:30:16 +0100

    wines index now uses foundation columns

--- app/assets/javascripts/user_wines_show/user_wines.js
+++ app/assets/javascripts/user_wines_show/user_wines.js
@@ -1,13 +1,8 @@
 $(document).ready(function() {
-
 // handles the add wine form appearing in user_wines/show
-
-		var form = $('#newNoteForm');
-
-		var button = $('#add_note_button');
-
-		button.on('click', function(){
-			form.toggle('slow');
-		})
-
+	var form = $('#newNoteForm');
+	var button = $('#add_note_button');
+	button.on('click', function(){
+		form.toggle('slow');
+	})
 });
